858b111cc8e1604df0456be79903a283bc5c2fbb
Steve Moser <contact@stevemoser.org>
2019-01-31 11:04:52 -0500

    Add danger-plugin-jira-issue

--- dangerfile.js
+++ dangerfile.js
@@ -6,4 +6,15 @@ if (danger.github.pr.body.length < 10) {
   warn('Please include a description of your PR changes.');
 }
 
-  
+import jiraIssue from "danger-plugin-jira-issue";
+
+jiraIssue({
+  key: "TJG",
+  url: "https://mosers.atlassian.net/browse",
+  emoji: ":paperclip:",
+  format(emoji, jiraUrls) {
+    // Optional Formatter
+    return "Some Custom Message";
+  },
+  location: "title" // Optional location, either 'title' or 'branch'
+});
